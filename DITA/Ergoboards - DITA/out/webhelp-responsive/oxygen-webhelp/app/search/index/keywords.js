0c407c6247e1a06f3539e2c7c0bffeb2528d0eb3
Magnus <michal.j.olender@gmail.com>
2022-03-25 23:48:27 +0100

    fixes

---
+++ DITA/Ergoboards - DITA/out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,14 @@
+define(function() {var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Atreus",p:["p1"]},{w:"Corne",p:["p2"]},{w:"Dygma",p:["p3"]},{w:"Raise",p:["p3"]},{w:"How",p:["p4"]},{w:"to",p:["p4"]},{w:"choose",p:["p4"]},{w:"your",p:["p4"]},{w:"first",p:["p4"]},{w:"board",p:["p4"]},{w:"Layouts",p:["p5"]},{w:"Moonlander",p:["p6"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3];
+ph["p2"]=[4];
+ph["p3"]=[5, 6];
+ph["p4"]=[7, 8, 9, 10, 11, 12];
+ph["p5"]=[13];
+ph["p6"]=[14];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ DITA/Ergoboards - DITA/out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {8:1,7:12,9:2,5:2,6:3,1:-1,3:-1,2:-1,11:12,4:1,12:-1,10:3};
+});
